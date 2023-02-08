6243a3ce160e5f4cb61c100dade39bd5d67ed402
Murder <87335384+MurderWolf@users.noreply.github.com>
2023-02-08 16:14:47 +0000

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,12 +5,12 @@ with open("README.md", "r", encoding="utf-8", errors="ignore") as fh:
 
 setuptools.setup(
     name="DiscordUtils",
-    version="1.3.6",
-    author="toxicrecker",
+    version="1.3.7",
+    author="Murder",
     description="DiscordUtils is a very useful library made to be used with discord.py",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://www.github.com/toxicrecker/DiscordUtils",
+    url="https://github.com/MurderWolf/DiscordUtils",
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
