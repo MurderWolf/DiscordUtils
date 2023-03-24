92a7cca12e23ce7407dd1f8732b6e38f1d055dca
Murder <87335384+MurderWolf@users.noreply.github.com>
2023-03-24 19:30:25 +0000

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8", errors="ignore") as fh:
 
 setuptools.setup(
     name="DiscordUtils",
-    version="1.3.9",
+    version="1.4.0",
     author="Murder",
     description="DiscordUtils is a very useful library made to be used with discord.py",
     long_description=long_description,
